107b7ec0deb2b9fc604bddc42cad9b8475582294
jhonatanjav <jhonjuaspuezan@gmail.com>
2022-09-08 18:01:49 -0500

    Uso de var de entoro con dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 app.use(express.static('public'));
 var hbs = require('hbs');
 
