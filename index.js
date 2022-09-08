11ff710729f39e2e445d90ba5c3664ae1c4c4617
jhonatanjav <jhonjuaspuezan@gmail.com>
2022-09-08 17:49:39 -0500

    version 1 server

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+app.use(express.static('public'));
+var hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+app.set('view engine', 'hbs');
+
+
+//respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.render('home', {
+    titulo: 'Topicos Avanzados',
+    nombre: 'prueba nombre'
+  });
+});
+
+app.get('/generic', function(req, res) {
+  res.render('generic', {
+    titulo: 'Topicos Avanzados',
+    nombre: 'Generic'
+  });
+});
+
+app.get('/elements', function(req, res) {
+  res.render('elements', {
+    titulo: 'Topicos Avanzados',
+    nombre: 'Elements'
+  });
+});
+
+
+
+app.listen(port , () => {
+
+  console.log(`escuchando por el puerto ${port}`)
+})
+
